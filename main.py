f0f3957b35ccc5cea39b3d38eb4c433e59251b22
studentgithub <atustudent62@gmail.com>
2024-07-26 09:09:31 -0700

    Data type

---
+++ main.py
@@ -0,0 +1,51 @@
+
+
+# Data type
+
+# String 
+
+# m =  "Arday"
+# print(m)
+
+# # type 
+
+# print(type(m))
+
+
+# Numberic (int, float)
+
+# i  = 10.0
+# print(type(i))
+
+
+# Boolean(true or false)
+
+
+# x  = 20
+# y = 40
+
+# result = x >  y
+# print(result)
+
+
+# Data structure(list, tuble , set dic)
+
+
+# z = ["Mohmed", "Khaalid", "muna", 1,2,3]
+# print(z[:1])
+
+# tuple
+
+# z = ("Mohmed", "Khaalid", "muna", 1,2,3)
+
+# print(type(z))
+
+# set
+
+# z = {"Mohmed", "Khaalid", "muna", 1,2,3}
+
+# print(type(z))
+
+
+m = {'name': "Khallid", "age": 20}
+print(type(m))
